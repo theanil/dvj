d8f1cf7acc798c97297e43582ba4f84eff411eb5
Anil <theanil@yahoo.com>
2017-05-05 17:48:24 +0530

    fgfgf

--- js/my-app.js
+++ js/my-app.js
@@ -131,11 +131,13 @@ document.addEventListener("deviceready", onDeviceReady, false);
 
         //alert(JSON.stringify(data));
 
-        //alert(data.message);
-        //alert('type: ' + data.additionalData.type);
-        //alert('id ' + data.additionalData.id);
-        //alert('id ' + data.id);
-        //alert('title ' + data.title);
+        alert(data.message);
+        alert('type: ' + data.additionalData.type);
+        alert('id ' + data.additionalData.id);
+        alert('id ' + data.id);
+        alert('title ' + data.title);
+        alert('category ' + data.category);
+        alert('vibrate ' + data.vibrate);
         //$("#password").val(data.message);
 
         message = data.message;
